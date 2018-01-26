6af4af5167331dd53493912983b46ba70fa897f5
AIE\gamejam <mitchellbell94@hotmail.com>
2018-01-26 22:07:34 +1100

    Added watersource and snaptogrid

---
+++ gamejam/Assets/Editor/SnapToGrid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class SnapToGrid : ScriptableWizard {
+
+	[MenuItem ("My Tools/Snap To Grid")]
+    static void SnapToGridWizard()
+    {
+        ScriptableWizard.DisplayWizard<SnapToGrid>("Snap To Grid", "Snap");
+
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject[] go = GameObject.FindObjectsOfType<GameObject>();
+        foreach(GameObject g in go)
+        {
+            g.transform.position = new Vector3(Mathf.Round(g.transform.position.x), Mathf.Round(g.transform.position.y), 0); 
+        }
+    }
+}
