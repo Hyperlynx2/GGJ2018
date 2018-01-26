211165fed911a81698ab065ed46f8a94295f58f2
AIE\gamejam <mitchellbell94@hotmail.com>
2018-01-26 21:38:09 +1100

    *Initial pipe inheritance structure. *gitignore and deleted stuff we don't need

---
+++ gamejam/Assets/Scripts/Tile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tile : MonoBehaviour
+{
+    //Constant
+    public int TIMETOFILL;
+
+    //Public variables
+    public bool up, down, left, right;          //Booleans for which directions the pipe leads.
+
+    //Private variables
+    protected int x, y;
+    protected TileManager tileManager;
+
+    void Start()
+    {
+        x = (int)Math.Round(this.transform.position.x);
+        y = (int)Math.Round(this.transform.position.y);
+        tileManager = GameObject.FindObjectOfType<TileManager>();
+        startTile();
+        tileManager.setTile(this, x, y);
+    }
+
+    void Update()
+    {
+        updateTile();
+    }
+
+    protected void propogateWater()
+    {
+        Tile nextTile;
+
+        if(up)
+        {
+            nextTile = tileManager.getTile(x, y - 1);
+            nextTile.startFilling();
+        }
+        if(down)
+        {
+            nextTile = tileManager.getTile(x, y + 1);
+            nextTile.startFilling();
+        }
+        if (left)
+        {
+            nextTile = tileManager.getTile(x - 1, y);
+            nextTile.startFilling();
+        }
+        if (right)
+        {
+            nextTile = tileManager.getTile(x + 1, y);
+            nextTile.startFilling();
+        }
+    }
+
+    public abstract void startTile();
+    public abstract void updateTile();
+    public abstract void startFilling();
+}
---
+++ gamejam/Assets/Scripts/waterSource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterSource : Tile {
+
+    //Public variables
+    public int timeTilStart;
+
+    //Private variables
+    private float countdownTimer;
+
+	// Use this for initialization
+	public override void startTile ()
+    {
+        countdownTimer = timeTilStart;
+	}
+	
+	// Update is called once per frame
+	public override void updateTile ()
+    {
+        countdownTimer -= Time.deltaTime;
+        if (countdownTimer <= 0)
+        {
+            propogateWater();
+        }
+	}
+
+    public override void startFilling()
+    {
+        return;
+    }
+}
