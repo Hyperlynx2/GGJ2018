77e0c6faf6a771cd47bc832771a48212b1be637d
lfrankel <leonard@leonardfrankel.com>
2018-01-28 10:51:50 +1100

    Made player2 green instead of blue

--- gamejam/Assets/Scripts/Goal.cs
+++ gamejam/Assets/Scripts/Goal.cs
@@ -6,18 +6,9 @@ public class Goal : Tile {
 
     public bool playerOne;
 
-    // Use this for initialization
     public override void startTile()
     {
-        //Sets colour of the goal based on which player it is for.
-        if(playerOne)
-        {
-            this.GetComponent<SpriteRenderer>().material.color = Color.red;
-        }
-        else
-        {
-            this.GetComponent<SpriteRenderer>().material.color = Color.blue;
-        }
+
     }
 
     // Update is called once per frame
